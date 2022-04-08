2298ef11b02f7673643298dc775dbbaeec5a7de9
ShahmarALiyev <saliyev2019@gmail.com>
2022-04-07 21:37:58 -0500

    Created navbar using mui-icons and styled components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 
-ReactDOM.render(
-  <React.StrictMode>
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+const app = (
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
+root.render(app);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
